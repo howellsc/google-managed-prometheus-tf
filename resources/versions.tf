6143b2bcdca502c561e5f7383ddf37d4d83cdf68
chrish <chris.d.howells@gmail.com>
2026-04-02 21:13:44 +0100

    Initial commit

--- resources/versions.tf
+++ resources/versions.tf
@@ -4,9 +4,5 @@ terraform {
       source  = "hashicorp/google"
       version = "7.26.0"
     }
-    # google-beta = {
-    #   source  = "hashicorp/google-beta"
-    #   version = "6.42.0"
-    # }
   }
 }
